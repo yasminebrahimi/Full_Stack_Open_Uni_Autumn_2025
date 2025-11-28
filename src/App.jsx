76896b5edc9c83363986dbdb2f94c863ecee8200
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2025-11-28 16:19:24 +0200

    Add solution for 1.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
+// const Content = (props) => {
+//   return (
+//     <div>
+//       <p>
+//         {props.part1} {props.exercises1}
+//       </p>
+//       <p>
+//         {props.part2} {props.exercises2}
+//       </p>
+//       <p>
+//         {props.part3} {props.exercises3}
+//       </p>
+//     </div>
+//   );
+// };
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+};
+
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part>
+        {" "}
+        part={props.part1} exercises={props.exercises1}
+      </Part>
+      <Part>
+        {" "}
+        part={props.part2} exercises={props.exercises2}
+      </Part>
+      <Part>
+        {" "}
+        part={props.part3} exercises={props.exercises3}
+      </Part>
     </div>
   );
 };
