23e6b191f029b43c236e0064a3708149b97bffef
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2025-11-30 17:05:21 +0200

    Add solution for 2.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-const Part = (props) => {
+const Header = ({ name }) => {
+  return <h1>{name}</h1>;
+};
+
+const Part = ({ part }) => {
   return (
     <p>
-      {props.part} {props.exercises}
+      {part.name} {part.exercises}
     </p>
   );
 };
@@ -9,52 +13,47 @@ const Part = (props) => {
 const Content = ({ parts }) => {
   return (
     <div>
-      <Part part={parts[0].name} exercises={parts[0].exercises} />
-      <Part part={parts[1].name} exercises={parts[1].exercises} />
-      <Part part={parts[2].name} exercises={parts[2].exercises} />
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
     </div>
   );
 };
 
-const Total = ({ parts }) => {
+//Course component as the parent component
+const Course = ({ course }) => {
   return (
-    <p>
-      Number of exes{" "}
-      {parts[0].exercises + parts[1].exercises + parts[2].exercises}
-    </p>
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </div>
   );
-};
-
-const Header = (props) => {
-  return <h1>{props.course}</h1>;
 };
 
 const App = () => {
   const course = {
+    id: 1,
     name: "Half Stack application development",
     parts: [
       {
         name: "Fundamentals of React",
         exercises: 10,
+        id: 1,
       },
       {
         name: "Using props to pass data",
         exercises: 7,
+        id: 2,
       },
       {
         name: "State of a component",
         exercises: 14,
+        id: 3,
       },
     ],
   };
 
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
+  return <Course course={course} />;
 };
 
 export default App;
